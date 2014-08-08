dc2dad22401e2a1b92e475c90baf7b5f9937b270
magnolium <magnolium1@gmail.com>
2014-08-08 00:02:10 -0400

    Player movement works

--- Platformer/Player.cpp
+++ Platformer/Player.cpp
@@ -11,10 +11,46 @@ Player::Player()
 
 void Player::Update()
 {
-	Player::objFrame = playerSheet->GetFrame("p1_walk10");
+	const Uint8 *state = SDL_GetKeyboardState(NULL);
+
+	if (!state[SDL_SCANCODE_RIGHT] || !state[SDL_SCANCODE_LEFT])
+	{
+		Player::objFrame = playerSheet->GetFrame("p1_stand");
+	}
+
+	if (!state[SDL_SCANCODE_RIGHT] || !state[SDL_SCANCODE_LEFT] || !state[SDL_SCANCODE_UP])
+	{
+		if (state[SDL_SCANCODE_DOWN])
+		{
+			Player::objFrame = playerSheet->GetFrame("p1_duck");
+			Player::posY += 1;
+		}
+	}
+
+	if (!state[SDL_SCANCODE_RIGHT] || !state[SDL_SCANCODE_LEFT] || !state[SDL_SCANCODE_DOWN])
+	{
+		if (state[SDL_SCANCODE_UP])
+		{
+			Player::objFrame = playerSheet->GetFrame("p1_jump");
+			Player::posY -= 1;
+		}
+	}
+
+	if (state[SDL_SCANCODE_LEFT])
+	{
+		Player::posX -= 1;
+		flip = true;
+		Player::objFrame = playerSheet->GetFrame(walkFrames[int(((SDL_GetTicks() / 25) % 11))]);
+	}
+	if (state[SDL_SCANCODE_RIGHT])
+	{
+		Player::posX += 1;
+		flip = false;
+		Player::objFrame = playerSheet->GetFrame(walkFrames[int(((SDL_GetTicks() / 25) % 11))]);
+	}
 }
 
 void Player::Draw(SDL_Renderer* pRenderer)
 {
-	TextureManager::Instance()->DrawTexture(Player::textureID, Player::posX, Player::posY, Player::objFrame.w, Player::objFrame.h, Player::objFrame.x, Player::objFrame.y, pRenderer);
+	TextureManager::Instance()->DrawTexture(Player::textureID, Player::posX, Player::posY, Player::objFrame.w, Player::objFrame.h, Player::objFrame.x, Player::objFrame.y, pRenderer, flip ? SDL_FLIP_HORIZONTAL : SDL_FLIP_NONE);
 }
--- Platformer/Player.h
+++ Platformer/Player.h
@@ -3,6 +3,7 @@
 #include "SpriteSheet.h"
 #include "SDL.h"
 #include <string>
+#include <array>
 
 
 class Player : public GameObject
@@ -15,7 +16,25 @@ public:
 	void Draw(SDL_Renderer* pRenderer);
 
 private:
-	std::string playerName;
 	SpriteSheet* playerSheet = 0;
+	std::string playerName;
+	bool flip = false;
+
+	std::array<std::string, 11> walkFrames
+	{
+		{
+			"p1_walk01",
+			"p1_walk02",
+			"p1_walk03",
+			"p1_walk04",
+			"p1_walk05",
+			"p1_walk06",
+			"p1_walk07",
+			"p1_walk08",
+			"p1_walk09",
+			"p1_walk10",
+			"p1_walk11"
+		}
+	};
 };
 
